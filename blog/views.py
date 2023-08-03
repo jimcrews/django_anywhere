1e94aa6ca390b801e36d453df934a2a75f438212
Jim Crews <crews.jim@gmail.com>
2023-08-04 06:38:27 +1000

    add commenting and sharing

--- blog/views.py
+++ blog/views.py
@@ -1,56 +1,64 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
-from .models import Post
+from django.views.decorators.http import require_POST
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
     """
     Alternative post list view
     """
+
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 3
-    template_name = 'blog/post/list.html'
+    template_name = "blog/post/list.html"
 
 
 def post_list(request):
     post_list = Post.published.all()
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
+    page_number = request.GET.get("page", 1)
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
         # If page_number is not an integer deliver the first page
         posts = paginator.page(1)
     except EmptyPage:
-        # If page_number is out of range deliver last page of results 
+        # If page_number is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, "blog/post/list.html", {"posts": posts})
 
 
 def post_detail(request, year, month, day, slug):
     try:
         post = Post.published.get(
-            slug=slug,
-            publish__year=year,
-            publish__month=month,
-            publish__day=day
+            slug=slug, publish__year=year, publish__month=month, publish__day=day
         )
     except Post.DoesNotExist:
         raise Http404("No Post Found")
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+
+    return render(
+        request,
+        "blog/post/detail.html",
+        {"post": post, "comments": comments, "form": form},
+    )
 
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     sent = False
-    if request.method == 'POST':
+    if request.method == "POST":
         # form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
@@ -59,9 +67,33 @@ def post_share(request, post_id):
             # send email
             post_url = request.build_absolute_uri(post.get_absolute_url())
             print(f"{cd['name']} recommends you read {post.title}")
-            print(f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}")
+            print(
+                f"Read {post.title} at {post_url}\n\n {cd['name']}'s comments: {cd['comments']}"
+            )
             sent = True
     else:
         form = EmailPostForm()
 
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        "blog/post/comment.html",
+        {"post": post, "form": form, "comment": comment},
+    )
